ae536e8e4718a1d2b98478f64dae2ad1c09faeb3
margiecalvert <margiecalvert@gmail.com>
2016-06-04 02:58:04 +0000

    this is good

--- ibitday/public/javascripts/first.js
+++ ibitday/public/javascripts/first.js
@@ -1,16 +1,24 @@
 var elfApp = angular.module('elfApp');
 
-elfApp.controller('FirstController', function($scope) {
+elfApp.controller('FirstController', function($scope, $http) {
     'use strict';
 
     $scope.description = 'FirstController Data';
 
+    $scope.loadData = function() {
+        $http.get('Presidents.json')
+            .then(function(presidents) {
+                $scope.presidents = JSON.stringify(presidents, null, 4);
+            })
+    };
+
+    $scope.loadData();
 });
 
-elfApp.directive('elfFirstDescription', function() {
+elfApp.directive('elfFirstData', function() {
     'use strict';
     return {
         controller: 'FirstController',
-        templateUrl: 'first-controller'
+        templateUrl: 'first-data'
     };
 });
